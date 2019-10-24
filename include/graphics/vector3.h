cdec6ef1b7d9beb3c612bb9da1bd07c8415aa44c
jake-bickle <bickle.jake@gmail.com>
2019-10-24 13:22:21 -0700

    Implement vector3 functionality

--- include/graphics/vector3.h
+++ include/graphics/vector3.h
@@ -1,12 +1,12 @@
 #pragma once
-
 #include <iostream>
 
 template <typename T>
 class Vector3{
     public:
         Vector3();
-        Vector3(T xx);
+        Vector3(const Vector3<T>& copy);
+        Vector3(T initalizer);
         Vector3(T x, T y, T z);
 
     private:
@@ -15,13 +15,17 @@ class Vector3{
         T z;
 
     public:
-        Vector3& normalize();  // Does this need to be Vector3f<T>& ?
+        Vector3<T>& normalize();  
         T dot(const Vector3<T>& vec) const;
-        T length_squared() const;
-        T length() const;
+        T magnitude_squared() const;
+        T magnitude() const;
         Vector3<T> operator * (const T& scalar) const;
-        Vector3<T> operator * (const Vector3<T>& rhs_vector) const;
+        Vector3<T> operator * (const Vector3<T>& rhs) const;
         Vector3<T>& operator *= (const T& scalar);
-        Vector3<T>& operator *= (const Vector3<T>& rhs_vector);
-        friend std::ostream operator << (std::ostream& os, const Vector3<T>& rhs_vector);
+        Vector3<T>& operator *= (const Vector3<T>& rhs);
+
+        friend std::ostream& operator << (std::ostream& os, const Vector3<T>& rhs){
+            os << "[" << rhs.x << " " << rhs.y << " " << rhs.z << "]";
+            return os;
+        }
 };
---
+++ src/vector3.cpp
@@ -0,0 +1,66 @@
+#include <graphics/vector3.h>
+#include <cmath>
+
+template <typename T>
+Vector3<T>::Vector3()
+    : x(0), y(0), z(0) {}
+
+template <typename T>
+Vector3<T>::Vector3(const Vector3<T>& copy)
+    : x(copy.x), y(copy.y), z(copy.z) {}
+
+template <typename T>
+Vector3<T>::Vector3(T initalizer)
+    : x(initalizer), y(initalizer), z(initalizer) {}
+
+template <typename T>
+Vector3<T>::Vector3(T x, T y, T z)
+    : x(x), y(y), z(z) {}
+
+template <typename T>
+Vector3<T>& Vector3<T>::normalize(){
+    *this *= (1 / this->magnitude());
+    return *this;
+}
+
+template <typename T>
+T Vector3<T>::dot(const Vector3<T>& vec) const{
+    return vec.x * x + vec.y * y + vec.z * z;
+}
+
+template <typename T>
+T Vector3<T>::magnitude_squared() const{
+    return this->dot(*this);
+}
+
+template <typename T>
+T Vector3<T>::magnitude() const{
+    return sqrt(this->magnitude_squared());
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator *= (const T& scalar){
+    this->x *= scalar;
+    this->y *= scalar;
+    this->z *= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator * (const T& scalar) const{
+    Vector3<T> new_vec(*this);
+    return new_vec *= scalar;
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator *= (const Vector3<T>& rhs){
+    this->x *= rhs.x;
+    this->y *= rhs.y;
+    this->z *= rhs.z;
+    return *this;
+}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator * (const Vector3<T>& rhs) const{
+    return Vector3<T>(*this) *= rhs;
+}
