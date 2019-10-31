cc4579444c40ba4fd1e9bf5876dd53af9dd0db44
jake-bickle <bickle.jake@gmail.com>
2019-10-31 12:27:12 -0700

    Create a simple scene with three spheres.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <graphics/shapes/sphere.h>
+#include <graphics/material.h>
+
+typedef Vector3<float> Vec3f;
 
 int main(){
-    std::cout << "Hello, world!\n";
+    Material green;
+    Material lightSource;
+    green.color = Vector3f(69, 252, 3);
+    lightSource.lightIntensity = 1;
+    std::vector<Shape3*> scene;
+    scene.push_back(new Sphere(Vector3f(10,0,10), 3, green));
+    scene.push_back(new Sphere(Vector3f(8,10,5), 3, green));
+    scene.push_back(new Sphere(Vector3f(0,20,0), 3, lightSource));
     return 0;
 }
