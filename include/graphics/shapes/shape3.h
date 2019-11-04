c0260e9b9ec27450d1da74f4243c8ab840c58ad1
jake-bickle <bickle.jake@gmail.com>
2019-11-04 15:17:46 -0800

    Change findIntersections to findNearestIntersection

--- include/graphics/shapes/shape3.h
+++ include/graphics/shapes/shape3.h
@@ -11,9 +11,7 @@ public:
     Shape3();
     Shape3(const Material& material);
     Shape3& setMaterial(const Material& material);
-    virtual bool findIntersections(const Vector3f& rayOrigin, const Vector3f& rayDirection, Vector3f& intersect1,
-                                   Vector3f& intersect2) const = 0;  
-    bool findNearestIntersection(const Vector3f& rayOrigin, const Vector3f& rayOirection, Vector3f& intersect);
+    virtual bool findNearestIntersection(const Vector3f& rayOrigin, const Vector3f& rayDirection, Vector3f& intersect) const = 0;
 };
 
 bool solveQuadratic(const float& a, const float& b, const float& c, float& x1, float& x2);
