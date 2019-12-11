9acc03c200c254361afc9becc7c8eee2911e2db5
jake-bickle <bickle.jake@gmail.com>
2019-12-11 14:44:19 -0800

    First working version of a render

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,27 @@
 #include <vector>
 #include <graphics/shapes/sphere.h>
 #include <graphics/material.h>
+#include <graphics/renderer.h>
+
+#include <ppm.h>
 
 typedef Vector3<float> Vec3f;
 
 int main(){
+    const char* filename = "/Users/jacobbickle/dev/cpp/graphics/my_first_render";
+
     Material green;
+    Material red;
     Material lightSource;
     green.color = Vector3f(69, 252, 3);
+    red.color = Vector3f(245, 90, 60);
     lightSource.lightIntensity = 1;
-    std::vector<Shape3*> scene;
-    scene.push_back(new Sphere(Vector3f(10,0,10), 3, green));
-    scene.push_back(new Sphere(Vector3f(8,10,5), 3, green));
-    scene.push_back(new Sphere(Vector3f(0,20,0), 3, lightSource));
+    Renderer r(800,600);
+    r.addShapeToScene(new Sphere(Vector3f(0,0,-10), 1, green));
+    r.addShapeToScene(new Sphere(Vector3f(1,1,-5), 1, red));
+    r.addShapeToScene(new Sphere(Vector3f(-1,-1,-5), 1, lightSource));
+
+    r.render(filename);
+
     return 0;
 }
